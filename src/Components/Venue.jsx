503a4604a6ad348bcccef5fcec29851237433c80
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-02-25 17:57:19 +0530

    venue section improved

--- src/Components/Venue.jsx
+++ src/Components/Venue.jsx
@@ -66,13 +66,11 @@ const Venue = () => {
               <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
                 {college.description}
               </p>
-              <TERipple>
                 <button
                   type="button"
-                  className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                  className="inline-block rounded p-3 px-10 text-sm font-medium  bg-[#1b1e1f] hover:bg-black cursor-pointer">
                   Book Now
                 </button>
-              </TERipple>
             </div>
           </div>
               </div>
